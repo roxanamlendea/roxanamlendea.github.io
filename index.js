993f897d0bfc699774c9f80be0455800ac9af6a7
Roxana Mlendea <roxanamlendea@gmail.com>
2023-06-16 17:42:21 +0300

    show only the content of the selected tabs

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 function showHome() {
   var page = document.getElementById("home");
   page.style.display = "block";
+  hideSkills();
+  hideKnownLanguages();
 }
 
 function showSkills() {
   var page = document.getElementById("skills");
   page.style.display = "block";
+  hideHome();
+  hideKnownLanguages();
+}
+
+function showKnownLanguages() {
+  var page = document.getElementById("languages");
+  page.style.display = "block";
+  hideHome();
+  hideSkills();
+}
+
+function hideHome() {
+  var page = document.getElementById("home");
+  page.style.display = "none";
+}
+
+function hideSkills() {
+  var page = document.getElementById("skills");
+  page.style.display = "none";
+}
+
+function hideKnownLanguages() {
+  var page = document.getElementById("languages");
+  page.style.display = "none";
 }
 
 showHome();
