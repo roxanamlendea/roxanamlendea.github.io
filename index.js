833effbc6cd4f4a1bf1d410ed41cce46cd59c802
Roxana Mlendea <roxanamlendea@gmail.com>
2023-06-16 19:09:43 +0300

    refactor hide all pages refore showing other pages

--- index.js
+++ index.js
@@ -1,42 +1,32 @@
+function hideAllPages() {
+  document.getElementById("home").style.display = "none";
+  document.getElementById("skills").style.display = "none";
+  document.getElementById("languages").style.display = "none";
+  document.getElementById("projects").style.display = "none";
+}
+
 function showHome() {
+  hideAllPages();
   var page = document.getElementById("home");
   page.style.display = "block";
-  hideSkills();
-  hideLanguages();
 }
 
 function showSkills() {
+  hideAllPages();
   var page = document.getElementById("skills");
   page.style.display = "block";
-  hideHome();
-  hideLanguages();
 }
 
 function showLanguages() {
+  hideAllPages();
   var page = document.getElementById("languages");
   page.style.display = "block";
-  hideHome();
-  hideSkills();
 }
 
 function showProjects() {
-  var page = document.getElementById("projects", page);
+  hideAllPages();
+  var page = document.getElementById("projects");
   page.style.display = "block";
-}
-
-function hideHome() {
-  var page = document.getElementById("home");
-  page.style.display = "none";
-}
-
-function hideSkills() {
-  var page = document.getElementById("skills");
-  page.style.display = "none";
-}
-
-function hideLanguages() {
-  var page = document.getElementById("languages");
-  page.style.display = "none";
 }
 
 showHome();
