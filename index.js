39a80ffbd973f63028503f88402f9353d33488c0
Roxana Mlendea <roxanamlendea@gmail.com>
2023-06-21 21:12:05 +0300

    save active menu

--- index.js
+++ index.js
@@ -8,7 +8,16 @@ function hide(id) {
 }
 
 function showPage(id) {
+  var oldLink = document.querySelector(
+    `#top-menu-bar a[data-page=${activePage}]`
+  );
+  oldLink.classList.remove("active");
+
   hide(activePage);
+
+  var link = document.querySelector(`#top-menu-bar a[data-page=${id}]`);
+  link.classList.add("active");
+
   var page = document.getElementById(id);
   console.info("show %o", id, page);
   page.style.display = "block";
@@ -30,5 +39,5 @@ function clickOnMenu(e) {
 
 // start our code
 
-showPage(activePage);
+showPage("home");
 document.getElementById("top-menu-bar").addEventListener("click", clickOnMenu);
